cd89013982b8035d725ddf4ae8934ee95a78d342
Administrator <756920137@qq.com>
2018-08-29 17:42:35 +0800

    df

---
+++ app/src/main/java/zhiren/gasdetection/AnJian/RefuseActivity.java
@@ -0,0 +1,18 @@
+package zhiren.gasdetection.AnJian;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import zhiren.gasdetection.R;
+
+/*
+* 到访不遇/拒绝入户
+* */
+public class RefuseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refuse);
+    }
+}
---
+++ app/src/main/java/zhiren/gasdetection/InstallService/InstallServiceActivity.java
@@ -0,0 +1,15 @@
+package zhiren.gasdetection.InstallService;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import zhiren.gasdetection.R;
+
+public class InstallServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_install_service);
+    }
+}
